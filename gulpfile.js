ecf425de2ce2ebd5506bb9403e2b1250e9031074
valentin74123 <58230964+valentin74123@users.noreply.github.com>
2021-05-22 23:23:36 +0500

    Корретирует Главную старницу по Pixel Perfect

--- gulpfile.js
+++ gulpfile.js
@@ -158,9 +158,9 @@ const reload = (done) => {
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/less/**/*.less", gulp.series("styles"));
+  gulp.watch("source/less/**/*.less", gulp.series(styles));
   gulp.watch("source/js/**/*.js", gulp.series(scripts));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/*.html", gulp.series(html, reload));
 }
 
 
